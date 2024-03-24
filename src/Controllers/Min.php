41105371ef81cdb473b35cde90affae9664911e4
tkirsch <mrmotts@gmail.com>
2024-03-23 21:01:12 -0400

    missing property

--- src/Controllers/Min.php
+++ src/Controllers/Min.php
@@ -1,9 +1,14 @@
-<?php namespace Tomkirsch\Assets\Controllers;
+<?php
+
+namespace Tomkirsch\Assets\Controllers;
+
 use CodeIgniter\Controller;
 
-class Min extends Controller{
-	
-	public function index(){
+class Min extends Controller
+{
+	protected $assets;
+	public function index()
+	{
 		$this->assets = service('assets');
 		$this->assets->runMinify(); // note that this will exit the script and ob_end_clean()
 	}
